e699c213610595afb740e72b8847e9fa4ca77ee0
SwampMaster2160 <jaxon1454@gmail.com>
2022-08-15 11:40:13 +1200

    6

---
+++ scripts/about.js
@@ -0,0 +1,14 @@
+function toggleCEOToMD() {
+    if (document.getElementById("ceoMDToggleButton").innerHTML == "See MD") {
+        document.getElementById("ceoName").innerHTML = "Managing Director - Bob Bob";
+        document.getElementById("ceoIntroduction").innerHTML = "The managing director since 2001, responsable for managing the company.";
+        document.getElementById("ceoMDToggleButton").innerHTML = "See CEO";
+        document.getElementById("ceoImage").src = "/images/md.jpg";
+    }
+    else {
+        document.getElementById("ceoName").innerHTML = "CEO - Bob Fakeson";
+        document.getElementById("ceoIntroduction").innerHTML = "\"I have been the ceo of this company since 2007, I am delighted to be able to steer this company to it's global corporation status today and are fully committed to further it's growth.\"";
+        document.getElementById("ceoMDToggleButton").innerHTML = "See MD";
+        document.getElementById("ceoImage").src = "/images/ceo.jpg";
+    }
+}
---
+++ scripts/contact.js
@@ -0,0 +1,10 @@
+function changeContactUsHeading() {
+    var contactUs = document.getElementById("contactUs");
+    contactUs.style.fontSize = "50px";
+    contactUs.style.color = "red";
+}
+
+function changeContactDetailsAlignment() {
+    var alignmentPadding = document.getElementById("alignmentPadding");
+    alignmentPadding.className = "col-4";
+}
